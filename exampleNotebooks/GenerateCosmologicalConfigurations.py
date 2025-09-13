8fdd011c4e30243568aa86f1b8198791550b3ee4
Robin Hoffmann <hoffmann.rob@gmx.de>
2025-09-13 12:43:45 +0200

    moved important definitions to top

--- exampleNotebooks/GenerateCosmologicalConfigurations.py
+++ exampleNotebooks/GenerateCosmologicalConfigurations.py
@@ -1,9 +1,20 @@
-
-# ## This Notebook calculates the powerspektrum for various diffrent cosmological starting parameters
-
-# %%
-import socket
-socket.gethostname()
+# Define dataset size
+N_total = 50000
+# Define parameter intervals
+Mth_min = 1e10
+Mth_max = 1e15
+param_intervals = {
+    'Om_c': (0.1, 0.6),
+    'Om_b': (0.04, 0.06),
+    'h': (0.64, 0.82),
+    'sigma_8': (0.8, 1),
+    'n_s': (0.84, 1.1),
+    'alpha': (0, 1),
+    'sigma': (0.01, 1),
+    'Mth': (Mth_min, Mth_max),
+    'Mprime': (Mth_min * 1, Mth_max * 100),
+    'beta': (0.1, 2)
+}
 
 # %%
 import pyccl as ccl
@@ -34,26 +45,10 @@ tf.random.set_seed(9721)
 # ## Calculating dataset
 
 # %%
-N_total = 10000000
 num_params = 10
 
 data_array = np.empty((N_total, num_params), dtype=object)
 
-Mth_min = 1e10
-Mth_max = 1e15
-# Definiere die Intervalle für die kosmologischen Parameter
-param_intervals = {
-    'Om_c': (0.1, 0.6),
-    'Om_b': (0.04, 0.06),
-    'h': (0.64, 0.82),
-    'sigma_8': (0.8, 1),
-    'n_s': (0.84, 1.1),
-    'alpha': (0, 1),
-    'sigma': (0.01, 1),
-    'Mth': (Mth_min, Mth_max),
-    'Mprime': (Mth_min * 1, Mth_max * 100),
-    'beta': (0.1, 2)
-}
 
 # Anzahl der Parameter (Dimensionen des LHS)
 num_cosmo_params = len(param_intervals)
